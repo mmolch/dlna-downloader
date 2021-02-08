bfc3e590a01e8319c4a53a75cba037b0ccb7118e
Moritz Molch <mail@moritzmolch.de>
2021-02-08 19:10:46 +0100

    Fix snap package

--- dev/snap/override-prime.sh
+++ dev/snap/override-prime.sh
@@ -14,9 +14,6 @@ install_package() {
 
 rm -rf *
 
-mkdir 'bin' 2>/dev/null
-cp -T "${SNAPCRAFT_PROJECT_DIR}/dev/snap/dlna-downloader" 'bin/dlna-downloader'
-
 while IFS= read -r path; do
     dir=${path%/*}
     echo "${path}"
@@ -27,6 +24,7 @@ done <<<$(cd "${SNAPCRAFT_PROJECT_DIR}/share" && find \( -name '*.py' -o -name '
 python3 -m compileall -b -r 10 .
 find -type f -name '*.py' -exec rm {} \;
 
+cp -r "${SNAPCRAFT_PROJECT_DIR}/bin" . 2>/dev/null
 cp -r "${SNAPCRAFT_STAGE}/data-dir" . 2>/dev/null
 cp -r "${SNAPCRAFT_STAGE}/snap" . 2>/dev/null
 
@@ -34,7 +32,6 @@ find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 chmod 755 bin/*
 chmod 755 snap/command-chain/*
-chmod 755 meta/hooks/*
 
 install_package python3-sip
 install_package python3-wxgtk4.0
@@ -42,7 +39,7 @@ install_package libwxgtk3.0-gtk3-0v5
 install_package libwxbase3.0-0v5
 install_package libpython3.6
 
-
+rm -rf 'data-dir'
 ls -la
 echo '##### PRIME END #####################################'
 exit 0
